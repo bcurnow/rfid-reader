585589167bc5d940c4319bb962622fb8b46e9432
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-04 17:32:58 +0000

    Added shebang

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 from setuptools import find_packages, setup
 
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(
