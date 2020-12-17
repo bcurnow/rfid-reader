9493e82ee107e9013c79cb702eca228e92cad1a1
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-17 19:14:13 +0000

    Imported exceptions into the main namespace, removed EVENT_READY_TIMEOUT as this is now a class variable for EvdevReader.

--- rfidreader/__init__.py
+++ rfidreader/__init__.py
@@ -1,3 +1,3 @@
 # Import into a single namespace
 from rfidreader.reader import RFIDReader  # noqa: F401
-from rfidreader.reader import EVENT_READY_TIMEOUT  # noqa: F401
+from rfidreader.exception import RFIDReaderException, RFIDReaderTypeException  # noqa: F401
