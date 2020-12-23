40d5f7b3cec6a6ce7db2eaad2b7ff5835cff5415
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-23 05:08:15 +0000

    updated to test an implementation based on the internal MFRC522 implementation that actually works with double uid PICCs

--- rfidreader/impl/mfrc522.py
+++ rfidreader/impl/mfrc522.py
@@ -1,8 +1,4 @@
-import atexit
-import time
-
-import RPi.GPIO as GPIO
-from mfrc522 import MFRC522
+from rfidreader.mfrc522 import MFRC522
 
 def register(config):
     return MFRC522Reader(config)
@@ -11,7 +7,6 @@ def register(config):
 class MFRC522Reader:
     def __init__(self, config):
         self.reader = MFRC522(**config)
-        atexit.register(GPIO.cleanup)
 
     def read(self, timeout=None):
         """
@@ -19,27 +14,4 @@ class MFRC522Reader:
         Returns None if timeout expires.
         If timeout is not provided, negative or None, will wait forever.
         """
-        # Default to no timeout
-        stop_time = None
-        if timeout and timeout >= 0:
-            stop_time = time.time() + timeout
-
-        id = self._read_id_no_block()
-        while not id and stop_time != time.time():
-            id = self._read_id_no_block()
-        return id
-
-    def _read_id_no_block(self):
-        (status, TagType) = self.reader.MFRC522_Request(self.reader.PICC_REQIDL)
-        if status != self.reader.MI_OK:
-            return None
-        (status, uid) = self.reader.MFRC522_Anticoll()
-        if status != self.reader.MI_OK:
-            return None
-        return self._uid_to_num(uid)
-
-    def _uid_to_num(self, uid):
-        n = 0
-        for i in range(0, 5):
-            n = n * 256 + uid[i]
-        return n
+        return self.reader.read_uid(timeout)
